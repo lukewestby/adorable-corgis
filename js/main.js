fa2fe40c4c9b0ebf19d1b4d922eec02a1803cc62
Luke William Westby <lwestby@alumni.nd.edu>
2014-07-19 16:54:29 -0500

    prevent scrolling on mobile

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ define([
 
 	var view = new CorgiCollectionView({ collection: corgiCollection });
 	Backbone.$('main').html(view.render().el);
+	
+	document.ontouchstart = function (ev) {
+		ev.preventDefault();
+	}
 
 	corgiCollection.fetch();
 });
